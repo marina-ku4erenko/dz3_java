9921b34c7fae98b22b996655fc0fdd661c7ea1f6
marina-ku4erenko <marina.ku4erenko@yandex.ru>
2021-11-18 23:44:21 +0300

    Залила решение доп. урока по Java

--- src/main/java/mari/ku/MainClass.java
+++ src/main/java/mari/ku/MainClass.java
@@ -3,21 +3,31 @@ package mari.ku;
 public class MainClass {
     public static void main(String[] args) {
 
-        Notebook samsung = new Notebook();
-        samsung.setBrand("Samsung");
-        samsung.setModel("GH666-1");
-        samsung.setPrice(69990);
-        samsung.applyDiscount30();
-        samsung.setRating(4);
+        Notebook lenovo = new Notebook("Lenovo", "JG-444", 65990);
+        lenovo.applyDiscount40();
+        lenovo.setRating(5);
+        lenovo.setDiagonal(22);
+        lenovo.printInfoNotebook();
+
+        Notebook dns = new Notebook("DD-222", 4);
+        dns.setBrand("DNS");
+        dns.setPrice(32590);
+        dns.applyDiscount30();
+        lenovo.setDiagonal(23);
+        dns.printInfoNotebook();
+
+        Notebook.changeCategory("Tablet");
+
+        Notebook samsung = new Notebook("Samsung", "L-123", 132490);
+        samsung.setRating(5);
         samsung.printInfoNotebook();
 
-        Notebook xiaomi = new Notebook();
+        Notebook xiaomi = new Notebook("FGH-556D", 3);
         xiaomi.setBrand("Xiaomi");
-        xiaomi.setModel("LL999-2w");
         xiaomi.setPrice(112990);
+        xiaomi.setDiagonal(25);
         xiaomi.applyDiscount40();
-        xiaomi.setRating(5);
+        xiaomi.setRating(4);
         xiaomi.printInfoNotebook();
-
     }
 }
--- src/main/java/mari/ku/Notebook.java
+++ src/main/java/mari/ku/Notebook.java
@@ -2,22 +2,46 @@ package mari.ku;
 
 public class Notebook {
 
-    String brand;
-    String model;
-    int price;
-    int discount;
-    int rating;
+    public static String category = "Notebook";
+    private String brand;
+    private String model;
+    private int price;
+    private int discount;
+    private int rating;
+
+    static class Monitor {
+        static int diagonal;
+    }
+
+    public Notebook(String brand, String model, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public Notebook(String model, int rating) {
+        this.model = model;
+        this.rating = rating;
+    }
+
+    private int getDiagonal() {
+        return Monitor.diagonal;
+    }
+
+    public void setDiagonal(int diagonal) {
+        Monitor.diagonal = diagonal;
+    }
 
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 
     public void applyDiscount30() {
@@ -30,16 +54,19 @@ public class Notebook {
         discount = 40;
     }
 
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
     public void printInfoNotebook() {
+        System.out.println("Категория товаров: " + category);
         System.out.println("Ноутбук: " + brand);
         System.out.println("Модель: " + model);
+        System.out.println("Дагональ экрана: " + getDiagonal());
         System.out.println("Цена: " + price);
         System.out.println("Размер применённой скидки: " + discount + "%");
         System.out.println("Рейтинг товара: " + rating + "/5");
-        System.out.println("");
+        System.out.println();
     }
+
+    public static void changeCategory(String newCategory) {
+        category = newCategory;
+    }
+
 }
